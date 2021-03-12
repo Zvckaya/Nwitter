ca5789c3f5c9ff20baac9881da1c0a62f0956bd2
허남규 <heonamgyu@heonamgyuui-MacBookAir.local>
2021-03-12 16:04:03 +0900

    create documnet on firestore

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
-import firebase from "firebase/app"
+import firebase from "firebase/app";
 import 'firebase/auth';
+import "firebase/firestore";
 
 
 
@@ -18,3 +19,5 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService =  firebase.auth();
+
+export const dbService = firebase.firestore();
